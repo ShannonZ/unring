5497c5f5c76e7788cf293969e432814cf01c29c0
Joseph Viviano <jviviano@bulbasaur.camhres.ca>
2017-10-06 16:13:49 -0400

    packaged and tested

--- matlab/unring.m
+++ matlab/unring.m
@@ -2,18 +2,25 @@
 %
 % unring - tool for removal of the Gibbs ringing artefact
 % Usage: outvol = unring(invol,params)
-% Options: invol - input volume 
+% Options: invol - input volume
 %          params - 3x1 array with [minW maxW nsh]
 %                     nsh discretization of subpixel spaceing (default 20)
 %                     minW  left border of window used for TV computation (default 1)
 %                     maxW  right border of window used for TV computation (default 3)
 
 
-function v = unring(v,params)
+function nrrd_out = unring(nrrd_in, nrrd_out, params);
+try
+    if nargin == 2,
+        params = [1 3 20];
+    end;
 
-if nargin == 1,
-    params = [1 3 20];
-end;
-
-v = double(v);
-v = ringRm(v,params);
+    dwi = nrrdLoadWithMetadata(nrrd_in);
+    dwi.data = double(dwi.data);
+    dwi.data = ringRm(dwi.data, params);
+    nrrdSaveWithMetadata(nrrd_out, dwi);
+    exit
+catch
+    exit(1)
+end
+end
